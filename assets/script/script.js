06d97cd5dc1dda95feeb6f7416540b0d8dac2401
AlexKryskevich <121296741+AlexKryskevich@users.noreply.github.com>
2023-02-17 17:29:55 +0300

    Test version

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+const menu_btn = document.getElementById('menu-btn');
+const menu_btn_icon = document.getElementById('menu-btn-icon')
+const menu = document.getElementById('nav-list');
+
+menu_btn.addEventListener('click', function() {
+    if (this.hasAttribute('closed')) {
+        this.removeAttribute('closed');
+        menu.style.transform = 'translateY(0)';
+        menu.style.transition = '.5s';
+        menu_btn_icon.classList.remove('fa-bars');
+        menu_btn_icon.classList.add('fa-x')
+    } else {
+        this.setAttribute('closed', '');
+        menu.style.transform = 'translateY(-100vh)';
+        menu.style.transition = '.5s';
+        menu_btn_icon.classList.remove('fa-x');
+        menu_btn_icon.classList.add('fa-bars');
+    }
+})
